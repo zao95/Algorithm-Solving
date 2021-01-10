a58c532d6adf2a6dcab00e37c579b481fc011930
zao95 <zao95@naver.com>
2021-01-11 01:30:00 +0900

    Update solves

---
+++ problem/JS100/009.js
@@ -0,0 +1,32 @@
+// 문제9 : concat을 활용한 출력 방법
+
+// 다음 소스 코드를 완성하여 날짜와 시간을 출력하시오.
+
+// ```jsx
+// **데이터**
+// var year = '2019';
+// var month = '04';
+// var day = '26';
+// var hour = '11';
+// var minute = '34';
+// var second = '27';
+
+// var result = //빈칸을 채워주세요
+
+// console.log(result);
+
+// **출력**
+// 2019/04/26 11:34:27
+// ```
+
+
+const year = '2019'
+const month = '04'
+const day = '26'
+const hour = '11'
+const minute = '34'
+const second = '27'
+
+const result = `${year}/${month}/${day} ${hour}:${minute}:${second}`
+
+console.log(result)
