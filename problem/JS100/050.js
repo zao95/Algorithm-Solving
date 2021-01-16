b585057a3f2bd9da9c89dcd488b58b440cdfc3c8
zao95 <zao95@naver.com>
2021-01-17 03:59:46 +0900

    Update solutions

---
+++ problem/JS100/050.js
@@ -0,0 +1,49 @@
+// 문제50 : 버블정렬 구현하기
+
+// 버블정렬은 두 인접한 원소를 검사하여 정렬하는 방법을 말합니다. 시간 복잡도는 느리지만 코드가 단순하기 때문에 자주 사용됩니다.
+
+// 아래 코드의 빈 칸을 채워 버블 정렬을 완성해 봅시다.
+
+// ```jsx
+// function bubble(arr) {
+//   let result = arr.slice(); 
+
+//   for (let i = 0; i < result.length - 1; i++) {
+//     for (/*빈칸을 채워주세요.*/) {
+//       if (result[j] > result[j + 1]) {
+//          //빈칸을 채워주세요.
+//       }
+//     }
+//   }
+//   return result;
+// }
+
+// const items = prompt('입력해주세요.').split(' ').map((n) => {
+//   return parseInt(n, 10);
+// });
+
+// console.log(bubble(items));
+// ```
+
+
+const bubble = (arr) => {
+    let result = arr.slice()
+
+    let temp
+    for (let i = 0; i < result.length - 1; i++) {
+        for (let j = 0; j < result.length - i - 1; j++) {
+            if (result[j] > result[j + 1]) {
+                temp = result[j]
+                result[j] = result[j + 1]
+                result[j + 1] = temp
+            }
+        }
+    }
+    return result
+}
+
+const items = [7, 5, 3, 8, 4].map((n) => {
+    return parseInt(n, 10);
+});
+
+console.log(bubble(items));
